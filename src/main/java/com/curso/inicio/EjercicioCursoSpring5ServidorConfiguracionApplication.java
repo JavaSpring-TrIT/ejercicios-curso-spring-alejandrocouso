c467f03ad5409bb351c271ea2956ca2feb20bb01
alejandrocouso <166011304+alejandrocouso@users.noreply.github.com>
2024-05-06 16:21:26 +0200

    first commit

---
+++ src/main/java/com/curso/inicio/EjercicioCursoSpring5ServidorConfiguracionApplication.java
@@ -0,0 +1,17 @@
+package com.curso.inicio;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class EjercicioCursoSpring5ServidorConfiguracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioCursoSpring5ServidorConfiguracionApplication.class, args);
+	}
+
+}

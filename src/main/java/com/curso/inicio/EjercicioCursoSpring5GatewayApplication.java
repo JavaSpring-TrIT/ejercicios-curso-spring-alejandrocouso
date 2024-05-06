4e817dbe1f8f2781679be08c8699d0a9c68bf936
alejandrocouso <166011304+alejandrocouso@users.noreply.github.com>
2024-05-06 16:17:12 +0200

    first commit

---
+++ src/main/java/com/curso/inicio/EjercicioCursoSpring5GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.curso.inicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioCursoSpring5GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioCursoSpring5GatewayApplication.class, args);
+	}
+
+}
